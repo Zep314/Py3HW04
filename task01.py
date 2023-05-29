476fe13048883ce47c1065fe646ad86d4dc3f566
Dmitry Mordanov <mordanov314@gmail.com>
2023-05-29 17:00:52 +0300

    middle01

--- task01.py
+++ task01.py
@@ -1,4 +1,51 @@
 # Напишите функцию для транспонирования матрицы
 
+
+def print_matrix(local_matrix):
+    """
+    Красиво выводим матрицу на печать
+    :param local_matrix:
+    :return:
+    """
+    for row in local_matrix:
+        print(f"{''.join(str(row))}")
+
+
+def transpose_matrix(local_matrix):
+    """
+    Возвращает транспонированную матицу
+    :param local_matrix:
+    :return:
+    """
+    return list(list(row) for row in zip(*local_matrix))
+
+
 if __name__ == '__main__':
-    pass
+    mmatrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [-9, 10, -11, 12],
+        [13, 14, 15, 16],
+    ]
+
+    print('Исходная матрица:')
+    print_matrix(mmatrix)
+    print('===============')
+    print('Транспонированная матрица:')
+    print_matrix(transpose_matrix(mmatrix))
+
+# Результат работы:
+# C:\Work\python\dz3\Py3HW04\venv\Scripts\python.exe C:/Work/python/dz3/Py3HW04/task01.py
+# Исходная матрица:
+# [1, 2, 3, 4]
+# [5, 6, 7, 8]
+# [-9, 10, -11, 12]
+# [13, 14, 15, 16]
+# ===============
+# Транспонированная матрица:
+# [1, 5, -9, 13]
+# [2, 6, 10, 14]
+# [3, 7, -11, 15]
+# [4, 8, 12, 16]
+#
+# Process finished with exit code 0
--- task03.py
+++ task03.py
@@ -2,5 +2,96 @@
 # - функции. Дополнительно сохраняйте все операции поступления и снятия средств
 # в список.
 
+# Напишите программу банкомат.
+# - Начальная сумма равна нулю
+# - Допустимые действия: пополнить, снять, выйти
+# - Сумма пополнения и снятия кратны 50 у.е.
+# - Процент за снятие — 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.
+# - После каждой третей операции пополнения или снятия начисляются проценты - 3%
+# - Нельзя снять больше, чем на счёте
+# - При превышении суммы в 5 млн, вычитать налог на богатство 10% перед каждой
+# операцией, даже ошибочной
+# - Любое действие выводит сумму денег
+
+from datetime import datetime
+
+PUSH_SUMM_MULTIPLE = 50
+PERCENT_FOR_OPERATION = 1.5
+MIN_SUMM_FOR_OPERATION = 30
+MAX_SUMM_FOR_OPERATION = 600
+BONUS_PERCENT = 3
+COUNT_OPERATION_FOR_BONUS = 3
+LUXURY_SUMM = 5_000_000
+LUXURY_PERCENT = 10
+
+def print_info():
+    print("""Программа - банкомат
+    Доступные команды:
+    /push <number> - пополнить счет на <number> денег 
+    /pop <number> - снять со счета <number> денег
+    /list - показать операции по счету
+    /exit - выход из программы
+    """)
+
+
+def push_account(money):
+    global account
+    global history
+    try:
+        if (int(money) % PUSH_SUMM_MULTIPLE == 0) and (int(money) > 0):
+            account += int(money)
+            history[datetime.now()] = ('Пополнение счета', money, account)
+        else:
+            print(f"Пополнять счет можно положительной суммой, кратной {PUSH_SUMM_MULTIPLE}")
+    except ValueError:
+        print("Ошибка ввода")
+
+
+def pop_account(money):
+    global account
+    global history
+    try:
+        if (int(money) > 0) and ((account - money) >= 0):
+            account -= int(money)
+            percent_fo_operation = money * PERCENT_FOR_OPERATION
+            if percent_fo_operation < MIN_SUMM_FOR_OPERATION:
+                percent_fo_operation = MIN_SUMM_FOR_OPERATION
+            if percent_fo_operation > MAX_SUMM_FOR_OPERATION:
+                percent_fo_operation = MAX_SUMM_FOR_OPERATION
+            account -= percent_fo_operation
+
+            history[datetime.now()] = ('Списание со счета', money, account)
+        else:
+            print("Снимать со счета можно положительную сумму, не превышая баланса")
+    except ValueError:
+        print("Ошибка ввода")
+
+
+def list_operations():
+    global history
+    for key_, value_ in history.items():
+        print(f'{key_}: {value_[0]}, {value_[1]:0.2f}. Баланс: {value_[2]:0.2f}')
+
+
 if __name__ == '__main__':
-    pass
+
+    print_info()
+    my_exit = False
+    account = 0
+    history = {}
+
+    while (not my_exit):
+        print(f'Баланс счета: {account:0.2f}')
+        inp = input(">>> ")
+        match inp.lower().split(' ')[0]:
+            case "/exit":
+                my_exit = True
+            case "/push":
+                push_account(inp.lower().split(' ')[1])
+            case "/pop":
+                pop_account(inp.lower().split(' ')[1])
+            case "/list":
+                list_operations()
+            case _:
+                print('Неизвестная команда')
+    print('Работа завершена')
